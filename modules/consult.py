902db5aa7f01262252a14d6283eb9a63678b4387
ChrisChV <xnpiochv@gmail.com>
2019-06-12 12:32:50 -0500

    functions.py and consult.py added

---
+++ modules/consult.py
@@ -0,0 +1,32 @@
+from ravegen import *
+import modules.sad as sad
+import bm_client
+import json
+
+@RaveGen
+@Command(description="Get info for all open transactions")
+def getOpens(message):
+    data = {}
+    data[sad._JSON_OPERATION_TYPE_] = sad._GET_OPEN_OPERATION_TYPE_
+    res_data = bm_client.sendData(data)
+    res_data  = json.loads(res_data)
+    return generateMessage(res_data)
+
+def generateMessage(res_data):
+    message = ""
+    for transaction_id, data in res_data.iteritems():
+        message += "Transaction " + str(transaction_id) + '\n'
+        message += "\tSymbol: " + data[sad._JSON_SYMBOL_] + '\n'
+        message += "\tFunction: " + data[sad._JSON_FUNCTION_] + '\n'
+        message += "\tState: " + data[sad._JSON_STATE_] + '\n'
+        message += "\tQuantity: " + str(data[sad._JSON_QUANTITY_]) + '\n'
+        message += "\tEntry Order:" + '\n'
+        message += "\t\tPrice: " + data[sad._JSON_ENTRY_] + '\n'
+        message += "\t\tState: " + data[sad._JSON_ENTRY_STATE_] + '\n'
+        message += "\tLose Order:" + '\n'
+        message += "\t\tPrice: " + data[sad._JSON_LOSE_] + '\n'
+        message += "\t\tState: " + data[sad._JSON_LOSE_STATE_] + '\n'
+        message += "\tProfit Order:" + '\n'
+        message += "\t\tPrice: " + data[sad._JSON_PROFIT_] + '\n'
+        message += "\t\tState: " + data[sad._JSON_PROFIT_STATE_] + '\n'
+    return message
---
+++ modules/funtions.py
@@ -0,0 +1,40 @@
+from ravegen import *
+import sad
+import bm_client
+
+
+@RaveGen
+@Command(description="Creates a new Transaction with simple function")
+def simple(message):
+    args = message.split()
+    return generateData(args, sad._FUNCTION_SIMPLE_)
+
+@RaveGen
+@Command(description="Creates a new Transaction with half function")
+def half(message):
+    args = message.split()
+    return generateData(args, sad._FUNCTION_HALF_)
+
+@RaveGen
+@Command(description="Creates a new Transaction with infiniteP function")
+def infinitep(message):
+    args = message.split()
+    return generateData(args, sad._FUNCTION_INFINITE_P_)
+
+
+def generateData(args, function):
+    if len(args) < 5:
+        return "Arguments missing <symbol> <entry> <lose> <profit> <quantity>"
+    if len(args) > 5:
+        return "Only 5 arguments <symbol> <entry> <lose> <profit> <quantity>"
+    data = {}
+    data[sad._JSON_OPERATION_TYPE_] = sad._NEW_OPERATION_TYPE_
+    data[sad._JSON_SYMBOL_] = args[0]
+    data[sad._JSON_ENTRY_] = float(args[1])
+    data[sad._JSON_LOSE_] = float(args[2])
+    data[sad._JSON_PROFIT_] = float(args[3])
+    data[sad._JSON_QUANTITY_] = float(args[4])
+    data[sad._JSON_FUNCTION_] = function
+    return bm_client.sendData(data)
+
+
